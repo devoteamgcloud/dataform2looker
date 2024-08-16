a45a27852ba70720cb3ea2ef9e3464d58a8e2956
papikiu <khachatur.papikyan@devoteam.com>
2024-08-16 20:08:59 +0200

    Add functionality to generate lookml views from dataform compiled result.json

--- dataform2looker/lookml.py
+++ dataform2looker/lookml.py
@@ -1,4 +1,7 @@
-import re
+import json
+import os
+
+from google.cloud import bigquery
 
 
 class Column:
@@ -7,120 +10,92 @@ class Column:
         self.type = type
         self.name = name
 
-    def get_description(self) -> str:
-        return self.description
 
-    def get_type(self) -> str:
-        return self.type
+class BigqueryTable:
+    def __init__(self, table_id: str) -> None:
+        self.table_id = table_id
+        self.columns = self.__create_columns_array()
 
-    def set_type(self, value: str) -> None:
-        self.type = value
+    def __create_columns_array(self) -> list[Column]:
+        client = bigquery.Client()
+        table = client.get_table(self.table_id)
+        columns = [
+            Column(field.description, field.field_type, field.name)
+            for field in table.schema
+        ]
+        return columns
 
-    def get_name(self) -> str:
-        return self.name
+    def get_number_of_columns(self) -> int:
+        return len(self.columns)
 
 
 class LookML:
-    def __init__(
-        self, path: str, table_name: str, description: str, columns: list[Column]
-    ) -> None:
+    def __init__(self, path: str) -> None:
         self.path = path
-        self.table_name = table_name
-        self.description = description
-        self.columns = columns
+        self.__tables_list = self.__read_json_into_list_of_tables()
+        self.__table_id_list = self.__create_list_of_table_ids()
+        self.__bigquery_table_info = (
+            self.__create_bigquery_table_id_to_columns_mapping()
+        )
+        self.__lookml_views_string = self.__generate_lookml_views()
 
-    def get_path(self) -> str:
-        return self.path
-
-    def get_config(self) -> str | None:
-        with open(self.path, "r") as file:
-            content = file.read()
-
-        # Match the config block using a regular expression
-        match = re.search(r"config \{\n(.*?)\}\n", content, re.DOTALL)
-
-        if match:
-            return match.group(1).strip()
-        else:
-            return None
-
-    def get_type(self) -> str | None:
-        config_block = self.get_config()
-
-        # Match the specific comment pattern within the config block
-        type_match = re.search(r"//df2lkml:type:(\w+)", config_block)
-
-        if type_match:
-            return type_match.group(1)
-        else:
-            return None
-
-    def get_column_name(self) -> str:
-        column_name = ""
-        return column_name
-
-    def get_column_description(self) -> str | None:
-        column_description = ""
-        return column_description
-
-    def create_column(self) -> Column:
-        return Column(
-            self.get_column_description(), self.get_type(), self.get_column_name()
+    def create_lookml_view_files(self) -> None:
+        lookml_directory_name = "views"
+        cwd = os.getcwd()
+        os.makedirs(lookml_directory_name, exist_ok=True)
+        view_definitions = self.__lookml_views_string.split("view: ")[1:]
+        for view_def in view_definitions:
+            view_name = view_def.split(" {")[0]
+            file_name = f"{view_name}.view.lkml"
+            file_path = os.path.join(lookml_directory_name, file_name)
+            with open(file_path, "w") as f:
+                f.write("view: " + view_def)
+        return print(
+            f"LookML view files successfully created in folder "
+            f"'{cwd}/{lookml_directory_name}'."
         )
 
-    def create_columns(self) -> list[Column]:
-        columns = []
-        print("IMPLEMENT ME!!! CREATE COLUMNS[]")
-        return columns
+    def __generate_lookml_views(self) -> str:
+        lookml_views = ""
+        for table_id, columns in self.__bigquery_table_info.items():
+            view_name = table_id.split(".")[-1]
+            lookml_views += f"view: {view_name} {{\n"
+            lookml_views += f"  sql_table_name: {table_id} ;;\n\n"
+            for column in columns:
+                lookml_views += f"  dimension: {column.name} {{\n"
+                lookml_views += f"    type: {column.type}\n"
+                lookml_views += f'    description: "{column.description}"\n'
+                lookml_views += "  }\n\n"
+            lookml_views += "}\n\n"
+        return lookml_views
 
-    # def generate(self) -> str:
-    #     """
-    #     Generates LookML code based on the provided table_sql.
-    #
-    #     Args:
-    #         None (uses self.table_sql)
-    #
-    #     Returns:
-    #         str: The LookML code representation.
-    #     """
-    #
-    #     # LookML View structure
-    #     lookml_code = f"view: {self.name} {{\n"
-    #
-    #     # Add derived table logic (assuming Dataform-style SQL)
-    #     lookml_code += "  derived_table: {\n"
-    #     lookml_code += f"    sql: {self.table_sql} ;;\n"
-    #     lookml_code += "  }\n\n"
-    #
-    #     # Define dimensions from columns
-    #     for col in self.columns:
-    #         lookml_code += f"  dimension: {col} {{\n"
-    #         lookml_code += "    type: string\n"  # TO BE ADJUSTED
-    #         lookml_code += f"    sql: ${{TABLE}}.{col} ;;\n"
-    #         lookml_code += "  }\n\n"
-    #
-    #     lookml_code += "}\n"
-    #
-    #     return lookml_code
+    def __create_bigquery_table_id_to_columns_mapping(self) -> dict[list[Column]]:
+        bigquery_table_info = {
+            table_id: BigqueryTable(table_id).columns
+            for table_id in self.__table_id_list
+        }
+        return bigquery_table_info
+
+    def __create_list_of_table_ids(self) -> list[str]:
+        table_id_list = [
+            (
+                table["target"]["database"]
+                + "."
+                + table["target"]["schema"]
+                + "."
+                + table["target"]["name"]
+            )
+            for table in self.__tables_list
+        ]
+        return table_id_list
+
+    def __read_json_into_list_of_tables(self) -> list[dict]:
+        with open(self.path, "r") as file:
+            data = json.load(file)
+            tables = data["tables"]
+            return tables
 
 
 if __name__ == "__main__":
-    column_vardavar = Column("My column description", "VARCHAR", "Vardavar")
-    column_amanor = Column("My column description", "VARCHAR", "Amanor")
-    print(
-        column_vardavar.get_name(),
-        column_vardavar.get_type(),
-        column_vardavar.get_description(),
-    )
-
-    path_to_sqlx = "/Users/khachatur/dataform2looker/example/synthetic_definition.sqlx"
-    table_name = "Festivities"
-    description = "My Festivities' description"
-    columns = [column_vardavar, column_amanor]
-
-    lookml_obj = LookML(path_to_sqlx, table_name, description, columns)
-
-    print(lookml_obj.get_config())
-    print(lookml_obj.get_type())
-    empty_column_type = lookml_obj.create_column()
-    print(empty_column_type.get_type())
+    lookml_object = LookML("./result.json")
+    lookml_object.create_lookml_view_files()
