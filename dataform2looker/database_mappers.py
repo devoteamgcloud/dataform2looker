d195b94805a9c986f3d65fb8dc00e59cb467e602
Edgar Ochoa <edgar.ochoa@devoteam.com>
2024-08-21 16:07:27 +0200

    breaking changes

--- dataform2looker/database_mappers.py
+++ dataform2looker/database_mappers.py
@@ -1,32 +1,41 @@
 from google.cloud import bigquery
 
-LOOKER_BIGQUERY_TYPE_MAP = {
-    "INT64": "number",
-    "INTEGER": "number",
-    "FLOAT": "number",
-    "FLOAT64": "number",
-    "NUMERIC": "number",
-    "BIGNUMERIC": "number",
-    "BOOLEAN": "yesno",
-    "STRING": "string",
-    "TIMESTAMP": "timestamp",
-    "DATETIME": "datetime",
-    "DATE": "date",
-    "TIME": "string",
-    "BOOL": "yesno",
-    "ARRAY": "string",
-    "GEOGRAPHY": "string",
-    "BYTES": "string",
-}
-
 class Column:
+
     def __init__(self, description: str, field_type: str, name: str, looker_type: str) -> None:
         self.description = description
         self.field_type = field_type
         self.name = name
         self.looker_type = looker_type
 
+class GenericTable:
+    def __init__(self, table_id:str, db_type:str) -> None:
+        if db_type =="bq":
+            self.table = BigqueryTable(table_id) 
+            # TODO find a better way to inherit the Class
+        else:
+            raise Exception(f"Table type not supported {db_type}")
+
 class BigqueryTable:
+    
+    _LOOKER_TYPE_MAP = {
+        "INT64": "number",
+        "INTEGER": "number",
+        "FLOAT": "number",
+        "FLOAT64": "number",
+        "NUMERIC": "number",
+        "BIGNUMERIC": "number",
+        "BOOLEAN": "yesno",
+        "STRING": "string",
+        "TIMESTAMP": "timestamp",
+        "DATETIME": "datetime",
+        "DATE": "date",
+        "TIME": "string",
+        "BOOL": "yesno",
+        "ARRAY": "string",
+        "GEOGRAPHY": "string",
+        "BYTES": "string",
+    }
     
     def __init__(self, table_id: str) -> None:
         self.table_id = table_id
@@ -41,5 +50,5 @@ class BigqueryTable:
         ]
         return columns
     
-    def __map_bigquery_to_looker_type(self, field_type: str) -> str:
-        return LOOKER_BIGQUERY_TYPE_MAP.get(field_type, "string")
+    def __map_to_looker_type(self, field_type: str) -> str:
+        return _LOOKER_TYPE_MAP.get(field_type, "string")
--- dataform2looker/lookml.py
+++ dataform2looker/lookml.py
@@ -1,62 +1,42 @@
 import json
 import os
-from database_mappers import BigqueryTable
+from database_mappers import GenericTable
+
 
 class LookML:
-    _database_types = ["bq"]
-
-    def __init__(self, path: str, database_type = "bq") -> None:
+    
+    def __init__(self, path: str, db_type: str, views_target_folder: str)-> None:
         self.path = path
-        self.database_type = database_type
-        self.tables_list = self.__get_list_of_tables()
-        self.__table_id_list = self.__create_list_of_table_ids()
-        self.__lookml_views_string = self.__generate_lookml_views()
+        self.db_type = db_type
+        self.tables_list = [
+            GenericTable(table_id, db_type)
+            for table_id in self.___get_table_ids()
+        ] #TODO Move it to a function Khachatur
+        self.lookml_template = None # TODO either use Jinja or use the example reference
+        self.views_target_folder = None 
 
-    def create_lookml_view_files(self) -> None:
-        lookml_directory_name = "views"
-        cwd = os.getcwd()
-        os.makedirs(lookml_directory_name, exist_ok=True)
-        view_definitions = self.__lookml_views_string.split("view: ")[1:]
-        for view_def in view_definitions:
-            view_name = view_def.split(" {")[0]
-            file_name = f"{view_name}.view.lkml"
-            file_path = os.path.join(lookml_directory_name, file_name)
-            with open(file_path, "w") as f:
-                f.write("view: " + view_def)
-        return print(
-            f"LookML view files successfully created in folder "
-            f"'{cwd}/{lookml_directory_name}'."
-        )
+    def __khachatur_fun1(self)->None:
+        # This function needs to save all of the templates that exist IN the LookML class
+        pass
 
-    def __generate_lookml_views(self) -> str:
-        lookml_views = ""
-        # for table_id, columns in self.__bigquery_table_info.items():
-        #     view_name = table_id.split(".")[-1]
-        #     lookml_views += f"view: {view_name} {{\n"
-        #     lookml_views += f"  sql_table_name: {table_id} ;;\n\n"
-        #     for column in columns:
-        #         lookml_views += f"  dimension: {column.name} {{\n"
-        #         lookml_views += f"    type: {column.looker_type}\n"
-        #         lookml_views += f'    description: "{column.description}"\n'
-        #         lookml_views += "  }\n\n"
-        #     lookml_views += "}\n\n"
-        return lookml_views
+    def __khachatur_fun1(self)->None:
+        # This function generates string of all of the templates 
+        # that exists in the class either from template or a python lib
+        pass
 
-    def __create_bigquery_table_id_to_columns_mapping(self) -> dict[list[Column]]:
-        bigquery_table_info = {
-            table_id: BigqueryTable(table_id).columns
-            for table_id in self.__table_id_list
-        }
-        return bigquery_table_info
-
-    def __create_list_of_table_ids(self) -> list:
-        if self.database_type == "bq":
-            table_list = [
-                BigqueryTable(f"{table["target"]["database"]}.{table["target"]["schema"]}.{table["target"]["name"]}")
-                for table in self.__tables_list
-            ]
-            
-        return table_list
+    def __get_list_of_tables(self) -> list[dict]:
+        with open(self.path, "r") as file:
+            data = json.load(file)
+            tables = data["tables"]
+            # TODO filter for tags, there should be a paremeter for this
+            return tables
+        
+    def ___get_table_ids(self) -> list[str]:
+        table_id_list = [
+            f"{table['database']}.{table['schema']}.{table['name']}"
+            for table in self.__get_list_of_tables()
+        ]
+        return table_id_list
 
     def __get_list_of_tables(self) -> list[dict]:
         with open(self.path, "r") as file:
@@ -65,7 +45,7 @@ class LookML:
             # TODO filter for tags, there should be a paremeter for this
             return tables
 
-
-if __name__ == "__main__":
-    lookml_object = LookML("./result.json")
-    lookml_object.create_lookml_view_files()
+if main="main"
+    
+    # Initialize LookML Class
+    # Save the data to a path using the intialized class
