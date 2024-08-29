9bc035d9df4604ffcde2f5083bffcef61256fe2b
papikiu <khachatur.papikyan@devoteam.com>
2024-08-29 18:41:46 +0200

    Minor naming changes

--- dataform2looker/dataform2looker.py
+++ dataform2looker/dataform2looker.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import os
 import subprocess
 import sys
 from collections.abc import Sequence
@@ -9,17 +8,16 @@ from pathlib import Path
 from dataform2looker.lookml import LookML
 
 
-def _generate_view(lookml_directory_name: str, target_dir: str) -> int:
+def _generate_view(path_to_json_file: str, target_dir: str) -> int:
     """Generates LookML view files from a Dataform model.
 
     Args:
-        lookml_directory_name (str): Path to the Dataform model file or directory.
+        path_to_json_file (str): Path to the JSON file from compiled Dataform project.
         target_dir (str): Target directory for Looker views.
     """
-    logging.info(f" Generating view for: {lookml_directory_name}")
+    logging.info(f" Generating views from: {path_to_json_file}")
     try:
-        # os.makedirs(lookml_directory_name, exist_ok=True)
-        lookml_object = LookML(lookml_directory_name, "BigQuery", target_dir)
+        lookml_object = LookML(path_to_json_file, "BigQuery", target_dir)
         lookml_object.save_lookml_views()
         return 0
     except subprocess.CalledProcessError as e:
@@ -37,13 +35,13 @@ def main(argv: Sequence[str] | None = None) -> int:
     )
 
     parser.add_argument(
-        "-dir",
+        "--source-path",
         type=Path,
         default=script_dir / "views",
         help="Path to the views file or directory containing views.",
     )
     parser.add_argument(
-        "-target-dir",
+        "--target-dir",
         type=Path,
         default=script_dir.parent / "views" / "output",
         help="Target directory for looker views. Default is 'views/output'.",
@@ -56,7 +54,7 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     args = parser.parse_args(argv)
 
-    input_path = args.dir
+    input_path = args.source_path
     target_dir = args.target_dir
     verbose = args.verbose
 
@@ -66,7 +64,7 @@ def main(argv: Sequence[str] | None = None) -> int:
         logging.info(f" Processing file: {input_path}")
         return _generate_view(str(input_path), str(target_dir))
     else:
-        logging.error("The provided path is neither a file nor a directory")
+        logging.error("The provided path is not taking to a JSON file")
         sys.exit(1)
 
 
