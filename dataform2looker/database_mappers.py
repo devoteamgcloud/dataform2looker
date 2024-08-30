16bc9962811b52a3a1d12e03b792d90ce663baa0
Edgar Ochoa <edgar.ochoa@devoteam.com>
2024-08-30 18:25:01 +0200

    code refactoring

--- dataform2looker/database_mappers.py
+++ dataform2looker/database_mappers.py
@@ -1,24 +1,98 @@
-from errors import UnsupportedDatabaseTypeError
+from exceptions import UnsupportedDatabaseTypeError
 from google.cloud import bigquery
 
 
 class Column:
+    _DIMENSION_TYPE_MAP = {
+        "number": "dimension",
+        "string": "dimension",
+        "timestamp": "time_dimension_group",
+        "datetime": "time_dimension_group",
+        "date": "time_dimension_group",
+        "yesno": "dimension",
+    }
+
+    _DIMENSION_GROUP_MAP = {
+        "timestamp": [
+            "raw",
+            "time",
+            "hour",
+            "date",
+            "week",
+            "month",
+            "quarter",
+            "year",
+        ],
+        "datetime": [
+            "raw",
+            "time",
+            "hour",
+            "date",
+            "week",
+            "month",
+            "quarter",
+            "year",
+        ],
+        "date": ["raw", "date", "week", "month", "quarter", "year"],
+    }
+
     def __init__(
         self, description: str, field_type: str, name: str, looker_type: str
     ) -> None:
-        self.description = description
+        self.description = description or ""
         self.field_type = field_type
         self.name = name
         self.looker_type = looker_type
+        self.column_dictionary = {
+            "name": self.name,
+            "type": self.field_type,
+            "description": self.description,
+            "sql": f"{{TABLE}}.{self.name}",
+        }
+        if self._DIMENSION_TYPE_MAP[self.looker_type] == "time_dimension_group":
+            self.column_dictionary["timeframes"] = self._DIMENSION_GROUP_MAP[
+                self.looker_type
+            ]
+            self.column_dictionary["datatype"] = (self.looker_type,)
+        self.field_looker_type = self._DIMENSION_TYPE_MAP[self.looker_type]
 
 
 class GenericTable:
     def __init__(self, table_id: str, db_type: str) -> None:
-        match db_type.lower():
-            case "bigquery":
-                self.table = BigqueryTable(table_id)
-            case _:
-                raise UnsupportedDatabaseTypeError(db_type)
+        if db_type == "bigquery":
+            self.__table = BigqueryTable(table_id)
+        else:
+            raise UnsupportedDatabaseTypeError(db_type)
+        self.table_id = table_id
+        self.table_name = self.__table.table_name
+        self.__db_type = db_type
+        # At the moment the dictionary for views and dimensions are built
+        # this is because the lkml lib requires the dict
+        # in case something different is used then we would need to
+        # re-factor the dictionary for GenericTable and Column
+        self.dimensions = [
+            column.column_dictionary
+            for column in self.__table.columns
+            if column.field_looker_type == "dimension"
+        ]
+        self.dimension_group = [
+            column.column_dictionary
+            for column in self.__table.columns
+            if column.field_looker_type == "time_dimension_group"
+        ]
+        # self.measures = # TODO set up measures
+        # All views should contain count
+        # TODO discuss if sum measures should be created automatically
+        # for for all type numbers
+        self.table_dictionary = {
+            "view": {
+                "name": f"{self.table_name}",
+                "sql_table_name": f"{table_id}",
+                "dimensions": self.dimensions,
+                "dimension_groups": self.dimension_group,
+                # "meausres": self.measures,
+            }
+        }
 
 
 class BigqueryTable:
@@ -61,4 +135,5 @@ class BigqueryTable:
         return columns
 
     def __map_to_looker_type(self, field_type: str) -> str:
-        return self._LOOKER_TYPE_MAP.get(field_type, "string")
+        # Avoid using get since default to "string" might not be the best option
+        return self._LOOKER_TYPE_MAP[field_type]
--- dataform2looker/lookml.py
+++ dataform2looker/lookml.py
@@ -1,128 +1,76 @@
 import json
+import logging
 import os
 
 import lkml
-from database_mappers import Column, GenericTable
+from database_mappers import GenericTable
 
 
 class LookML:
     def __init__(
-        self, source_json_path: str, db_type: str, views_target_folder: str
+        self,
+        source_json_path: str,
+        db_type: str,
+        views_target_folder: str,
+        tags: list[str] = None,
     ) -> None:
         self.path = source_json_path
         self.db_type = db_type
-        self.tables_list = self.__create_tables()
-        self.lookml_templates = self.__generate_lookml_templates()
+        self.__tables_ids = self.__get_list_of_table_ids()
+        self.__tables_list = self.__initialize_tables(self.__tables_ids)
+        self.lookml_templates = self.__generate_lookml_templates(self.__tables_list)
         self.views_target_folder = views_target_folder
+        # TODO finish setting up tags to be used as filters
+        # when getting the list of tables
+        self.tags = set(tags)
 
     def save_lookml_views(self) -> None:
-        for index, template in enumerate(self.lookml_templates):
-            file_path = (
-                f"{self.views_target_folder}/"
-                f"{self.tables_list[index].table.table_name}.view.lkml"
-            )
+        for table_name, table_template in self.lookml_templates.items():
+            file_path = f"{self.views_target_folder}/" f"{table_name}.view.lkml"
             with open(file_path, "w") as f:
-                f.write(template)
-        return print(
-            f"LookML view files successfully created in folder "
-            f"'{self.views_target_folder}'"
+                f.write(table_template)
+        logging.info(
+            f"A total of {len(self.lookml_templates)} LookML view files successfully \
+                created in folder '{self.views_target_folder}'"
         )
 
-    def __generate_lookml_templates(self) -> list[str]:
-        lookml_views_list = []
-        for table in self.tables_list:
-            lookml_view = self.__create_lookml_view(table)
-            lookml_views_list.append(lkml.dump(lookml_view))
-        return lookml_views_list
+    def __generate_lookml_templates(self, tables_list: list[GenericTable]) -> dict:
+        lookml_tables = {}
+        for table in tables_list:
+            lookml_tables[table.table_name] = lkml.dump(table.table_dictionary)
+            # TODO check if we should use lkml dump to create the file
+            # If we want to control the saving of the file might be easier
+            # to do it outside the lib
+            # https://lkml.readthedocs.io/en/latest/lkml.html#module-lkml
+        return lookml_tables
 
-    def __create_lookml_view(self, table: GenericTable) -> dict:
-        dimensions_list, datetime_list, date_list = self.__categorize_columns(table)
-
-        return {
-            "view": {
-                "name": f"{table.table.table_name}",
-                "sql_table_name": f"{table.table.table_id}",
-                "dimensions": dimensions_list,
-                "dimension_group": datetime_list + date_list,
-            }
-        }
-
-    def __categorize_columns(self, table: GenericTable) -> tuple[list, list, list]:
-        dimensions_list = []
-        datetime_list = []
-        date_list = []
-
-        for column in table.table.columns:
-            match column.looker_type:
-                case "timestamp" | "datetime":
-                    datetime_list.append(
-                        self.__create_time_dimension(
-                            column,
-                            [
-                                "raw",
-                                "time",
-                                "hour",
-                                "date",
-                                "week",
-                                "month",
-                                "quarter",
-                                "year",
-                            ],
-                        )
-                    )
-                case "date":
-                    date_list.append(
-                        self.__create_time_dimension(
-                            column, ["raw", "date", "week", "month", "quarter", "year"]
-                        )
-                    )
-                case _:
-                    dimensions_list.append(self.__create_dimension(column))
-
-        return dimensions_list, datetime_list, date_list
-
-    def __create_time_dimension(self, column: Column, timeframes: list[str]) -> dict:
-        return {
-            "type": "time",
-            "description": f"{column.description}",
-            "name": f"{column.name}",
-            "datatype": f"{column.looker_type}",
-            "timeframes": timeframes,
-            "sql": f"{{TABLE}}.{column.name}",
-        }
-
-    def __create_dimension(self, column: Column) -> dict:
-        return {
-            "type": f"{column.looker_type}",
-            "description": f"{column.description}",
-            "name": f"{column.name}",
-            "sql": f"{{TABLE}}.{column.name}",
-        }
-
-    def __create_tables(self) -> list[GenericTable]:
-        tables_list = [
-            GenericTable(table_id, self.db_type) for table_id in self.__get_table_ids()
-        ]
+    def __initialize_tables(
+        self,
+        tables_ids: list[str],
+    ) -> list[GenericTable]:
+        tables_list = [GenericTable(table_id, self.db_type) for table_id in tables_ids]
         return tables_list
 
-    def __get_table_ids(self) -> list[str]:
-        table_id_list = [
-            f"{table['target']['database']}.{table['target']['schema']}.{table['target']['name']}"
-            for table in self.__get_list_of_tables()
-        ]
-        return table_id_list
-
-    def __get_list_of_tables(self) -> list[dict]:
+    def __get_list_of_table_ids(self) -> list[dict]:
         with open(self.path, "r") as file:
             data = json.load(file)
             tables = data["tables"]
-            return tables
+        table_id_list = [
+            f"{table['target']['database']}.{table['target']['schema']}.{table['target']['name']}"
+            for table in tables
+        ]
+        logging.debug(f"Read file {self.path}, found {len(tables)}")
+        # TODO implement tag filter
+        # pass the following parameter tags:set[str] = None
+        # use this as a filter [
+        # x for table in tables if self.tags.intersection(set(table["tags"]))
+        return table_id_list
 
 
 if __name__ == "__main__":
     lookml_directory_name = "views"
     os.makedirs(lookml_directory_name, exist_ok=True)
     lookml_object = LookML(
-        "./result.json", "BigQuery", f"{os.getcwd()}/{lookml_directory_name}"
+        "./result.json", "bigquery", f"{os.getcwd()}/{lookml_directory_name}"
     )
     lookml_object.save_lookml_views()
