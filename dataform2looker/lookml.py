7ddd676aaa361977cfaf098a37aeb4462703c96e
papikiu <khachatur.papikyan@devoteam.com>
2024-08-30 12:40:29 +0200

    Refactor + Add helper functions to generate lookml templates

--- dataform2looker/lookml.py
+++ dataform2looker/lookml.py
@@ -2,19 +2,21 @@ import json
 import os
 
 import lkml
-from database_mappers import GenericTable
+from database_mappers import Column, GenericTable
 
 
 class LookML:
-    def __init__(self, path: str, db_type: str, views_target_folder: str) -> None:
-        self.path = path
+    def __init__(
+        self, source_json_path: str, db_type: str, views_target_folder: str
+    ) -> None:
+        self.path = source_json_path
         self.db_type = db_type
         self.tables_list = self.__create_tables()
-        self.lookml_template = self.__generate_lookml_templates()
+        self.lookml_templates = self.__generate_lookml_templates()
         self.views_target_folder = views_target_folder
 
     def save_lookml_views(self) -> None:
-        for index, template in enumerate(self.lookml_template):
+        for index, template in enumerate(self.lookml_templates):
             file_path = (
                 f"{self.views_target_folder}/"
                 f"{self.tables_list[index].table.table_name}.view.lkml"
@@ -29,65 +31,73 @@ class LookML:
     def __generate_lookml_templates(self) -> list[str]:
         lookml_views_list = []
         for table in self.tables_list:
-            dimensions_list = []
-            datetime_list = []
-            date_list = []
-            for column in table.table.columns:
-                match column.looker_type:
-                    case "timestamp" | "datetime":
-                        datetime_list.append(
-                            {
-                                "type": "time",
-                                "description": f"{column.description}",
-                                "name": f"{column.name}",
-                                "datatype": f"{column.looker_type}",
-                                "timeframes": [
-                                    "raw",
-                                    "time",
-                                    "hour",
-                                    "date",
-                                    "week",
-                                    "month",
-                                    "quarter",
-                                    "year",
-                                ],
-                            }
-                        )
-                    case "date":
-                        date_list.append(
-                            {
-                                "type": "time",
-                                "description": f"{column.description}",
-                                "name": f"{column.name}",
-                                "datatype": f"{column.looker_type}",
-                                "timeframes": [
-                                    "raw",
-                                    "date",
-                                    "week",
-                                    "month",
-                                    "quarter",
-                                    "year",
-                                ],
-                            }
-                        )
-                    case _:
-                        dimensions_list.append(
-                            {
-                                "type": f"{column.looker_type}",
-                                "description": f"{column.description}",
-                                "name": f"{column.name}",
-                            }
-                        )
-            lookml_view = {
-                "view": {
-                    "name": f"{table.table.table_name}",
-                    "sql_table_name": f"{table.table.table_id}",
-                    "dimensions": dimensions_list,
-                    "dimension_group": datetime_list + date_list,
-                }
-            }
+            lookml_view = self.__create_lookml_view(table)
             lookml_views_list.append(lkml.dump(lookml_view))
         return lookml_views_list
+
+    def __create_lookml_view(self, table: GenericTable) -> dict:
+        dimensions_list, datetime_list, date_list = self.__categorize_columns(table)
+
+        return {
+            "view": {
+                "name": f"{table.table.table_name}",
+                "sql_table_name": f"{table.table.table_id}",
+                "dimensions": dimensions_list,
+                "dimension_group": datetime_list + date_list,
+            }
+        }
+
+    def __categorize_columns(self, table: GenericTable) -> tuple[list, list, list]:
+        dimensions_list = []
+        datetime_list = []
+        date_list = []
+
+        for column in table.table.columns:
+            match column.looker_type:
+                case "timestamp" | "datetime":
+                    datetime_list.append(
+                        self.__create_time_dimension(
+                            column,
+                            [
+                                "raw",
+                                "time",
+                                "hour",
+                                "date",
+                                "week",
+                                "month",
+                                "quarter",
+                                "year",
+                            ],
+                        )
+                    )
+                case "date":
+                    date_list.append(
+                        self.__create_time_dimension(
+                            column, ["raw", "date", "week", "month", "quarter", "year"]
+                        )
+                    )
+                case _:
+                    dimensions_list.append(self.__create_dimension(column))
+
+        return dimensions_list, datetime_list, date_list
+
+    def __create_time_dimension(self, column: Column, timeframes: list[str]) -> dict:
+        return {
+            "type": "time",
+            "description": f"{column.description}",
+            "name": f"{column.name}",
+            "datatype": f"{column.looker_type}",
+            "timeframes": timeframes,
+            "sql": f"{{TABLE}}.{column.name}",
+        }
+
+    def __create_dimension(self, column: Column) -> dict:
+        return {
+            "type": f"{column.looker_type}",
+            "description": f"{column.description}",
+            "name": f"{column.name}",
+            "sql": f"{{TABLE}}.{column.name}",
+        }
 
     def __create_tables(self) -> list[GenericTable]:
         tables_list = [
