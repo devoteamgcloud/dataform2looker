dcf9500b07812ea82b8f53f1cc209d5ef947a7e4
Edgar Ochoa <edgar.ochoa@devoteam.com>
2024-09-16 14:12:10 +0200

    update documentation and argument df2looker

--- dataform2looker/dataform2looker.py
+++ dataform2looker/dataform2looker.py
@@ -16,6 +16,9 @@ def _generate_view(path_to_json_file: str, target_dir: str) -> int:
     Args:
         path_to_json_file (str): Path to the JSON file from compiled Dataform project.
         target_dir (str): Target directory for Looker views.
+
+    Returns:
+        int: 0 if the view generation was successful, 1 otherwise.
     """
     logging.info(f" Generating views from: {path_to_json_file}")
     try:
@@ -23,12 +26,16 @@ def _generate_view(path_to_json_file: str, target_dir: str) -> int:
         lookml_object.save_lookml_views()
         return 0
     except subprocess.CalledProcessError as e:
-        logging.error(f"I failed....: {e}")
+        logging.error(f"I failed...: {e}")
         return 1
 
 
 def main(argv: Sequence[str] | None = None) -> int:
-    """Main function for the CLI script."""
+    """Main function for the CLI script.
+
+    Returns:
+        int: 0 if the script runs successfully, 1 otherwise.
+    """
     script_dir = Path(__file__).resolve().parent
 
     argv = argv if argv is not None else sys.argv[1:]
@@ -39,14 +46,13 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument(
         "--source-path",
         type=Path,
-        default=script_dir / "views",
         help="Path to the views file or directory containing views.",
     )
     parser.add_argument(
         "--target-dir",
         type=Path,
-        default=script_dir.parent / "views" / "output",
-        help="Target directory for looker views. Default is 'views/output'.",
+        default=script_dir.parent / "views",
+        help="Target directory for looker views. Default is 'views'.",
     )
     parser.add_argument(
         "--verbose",
@@ -65,10 +71,5 @@ def main(argv: Sequence[str] | None = None) -> int:
     if input_path.is_file():
         logging.info(f" Processing file: {input_path}")
         return _generate_view(str(input_path), str(target_dir))
-    else:
-        logging.error("The provided path is not taking to a JSON file")
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
+    logging.error("The provided path is not taking to a JSON file")
+    sys.exit(1)
