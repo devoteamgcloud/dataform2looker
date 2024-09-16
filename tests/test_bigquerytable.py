270e4659bc3f84f4cdd4a5efc14e4796cd3e4d1e
Edgar Ochoa <edgar.ochoa@devoteam.com>
2024-09-16 12:06:12 +0200

    add assertions

--- tests/test_bigquerytable.py
+++ tests/test_bigquerytable.py
@@ -10,7 +10,11 @@ class TestBigQueryTable:
 
     @pytest.fixture()
     def my_bq_table(self, bq_table_id: str) -> BigQueryTable:
-        """Creates a `BigqueryTable` object for testing."""
+        """Creates a `BigqueryTable` object for testing.
+
+        Returns:
+            BigQueryTable: A `BigQueryTable` object initialized with the provided `bq_table_id`.
+        """  # noqa: E501
         return BigQueryTable(bq_table_id)
 
     def test_init(self, my_bq_table: BigQueryTable, bq_table_id: str) -> None:
@@ -19,6 +23,8 @@ class TestBigQueryTable:
         Verifies that the attributes of the created `BigQueryTable` object match the expected values.
         """  # noqa: E501
         assert my_bq_table.table_id == bq_table_id
+        assert my_bq_table.table_name == bq_table_id.split(".")[-1]
+        assert isinstance(my_bq_table.columns, list)
 
 
 # TODO add other tests for BigQueryTable class.
