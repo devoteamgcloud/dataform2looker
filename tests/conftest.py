eb378c5585bf4da06ffa9213796f116adc5a79aa
Edgar Ochoa <edgar.ochoa@devoteam.com>
2024-09-16 12:01:01 +0200

    adding pytest conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,25 @@ from pytest import FixtureRequest, Parser, fixture
 
 
 def pytest_addoption(parser: Parser) -> None:
-    """Fixture bq_table_id parser."""
+    """Fixture parsers."""
     parser.addoption(
         "--bq_table_id",
         action="store",
         default="bigquery-public-data.chicago_taxi_trips.taxi_trips",
-        help="bigqueyr table id to use for testing, \
-              use chicago taxi trips schema from public data in BigQuery",
+        help="BigQuery table id to use for testing, \
+              use chicago taxi trips schema from public data in BigQuery.",
+    )
+    parser.addoption(
+        "--source_json_path",
+        action="store",
+        default="tests/dataform_result.json",
+        help="Path to the Dataform JSON file used for testing.",
+    )
+    parser.addoption(
+        "--target_folder_path",
+        action="store",
+        default="tests/",
+        help="Target path for the generated views.",
     )
 
 
@@ -18,3 +30,15 @@ def pytest_addoption(parser: Parser) -> None:
 def bq_table_id(request: FixtureRequest) -> str:
     """Fixture bq_table_id."""
     return request.config.getoption("--bq_table_id")
+
+
+@fixture()
+def source_json_path(request: FixtureRequest) -> str:
+    """Fixture source_json_path."""
+    return request.config.getoption("--source_json_path")
+
+
+@fixture()
+def target_folder_path(request: FixtureRequest) -> str:
+    """Fixture target_folder_path."""
+    return request.config.getoption("--target_folder_path")
